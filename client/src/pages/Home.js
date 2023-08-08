198f5d9bb6625746ddddbbaf11f56b63b73d5bbd
utkarsh-radiant <utkarsh.uitest@gmail.com>
2023-08-08 10:20:51 +0530

    client file

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "./Layout";
+
+const Home = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>My Home</h1>
+      </Layout>
+    </div>
+  );
+};
+
+export default Home;
